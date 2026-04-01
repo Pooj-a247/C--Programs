751bf16ee8b042a8d97a24ac99e5896ae7142b8d
Pooj-a247 <pooja24hi@gmail.com>
2026-04-01 10:09:50 +0530

    add pattern prgm2

---
+++ numbers/inverted_right_angled_triangle.c
@@ -0,0 +1,27 @@
+/*
+Example:
+Input: 5
+Output:
+*****
+****
+***
+**
+*
+*/
+
+#include <stdio.h>
+int main()
+{
+  int rows, i, j;
+  printf("Enter number of rows: ");
+  scanf("%d", &rows);
+  for (i = rows; i >= 1; i--) 
+  {
+    for (j = 1; j <= i; j++) 
+    {
+      printf("*");
+    }
+    printf("\n");
+  }
+  return 0;
+}
